c21653ebf2b12f9ac07f1789e172e8017d061be9
Orhan Polat <orhanpolat@gmail.com>
2023-03-22 21:57:59 +0100

    openapi implementation bugfix of multiple join with same table

--- src/main/java/nl/abnamro/assessment/recipe/controller/RecipeController.java
+++ src/main/java/nl/abnamro/assessment/recipe/controller/RecipeController.java
@@ -1,6 +1,13 @@
 package nl.abnamro.assessment.recipe.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import nl.abnamro.assessment.recipe.domain.Recipe;
 import nl.abnamro.assessment.recipe.enums.SearchOperation;
 import nl.abnamro.assessment.recipe.message.RestResponse;
 import nl.abnamro.assessment.recipe.model.RecipeDto;
@@ -28,42 +35,101 @@ public class RecipeController {
 
     private final IRecipeService recipeService;
 
+    @Operation(summary = "Get all existing recipes")
+    @ApiResponse(responseCode = "200", description = "Found the recipes",
+            content = { @Content(mediaType = "application/json",
+            schema = @Schema(implementation = Recipe.class))})
     @GetMapping
     public ResponseEntity<RestResponse> findAll() {
         LOG.info("Get All Recipe Information..");
         return ResponseEntity.ok(recipeService.findAll());
     }
 
+    @Operation(summary = "Save recipe with given Request Body")
+    @ApiResponses( value = {
+            @ApiResponse(responseCode = "201", description = "Recipe created",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Recipe.class))}),
+            @ApiResponse(responseCode = "400" , description = "Recipe could not saved",
+                    content= @Content)
+    })
     @PostMapping
-    public ResponseEntity<RestResponse> saveRecipe(@RequestBody @Validated RecipeDto recipeDto) {
+    public ResponseEntity<RestResponse> saveRecipe(@Parameter(description = "Request of recipe to be saved")
+                                                       @RequestBody @Validated RecipeDto recipeDto) {
         LOG.info("Save New Recipe");
         return ResponseEntity.ok(recipeService.saveRecipe(recipeDto));
     }
 
+    @Operation(summary = "Update recipe by its id")
+    @ApiResponses( value = {
+            @ApiResponse(responseCode = "204", description = "Recipe updated",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Recipe.class))}),
+            @ApiResponse(responseCode = "406", description = "Recipe IDs mismatch!",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "No Recipe data found with given ID",
+                    content = @Content),
+            @ApiResponse(responseCode = "400" , description = "Recipe could not updated",
+                    content= @Content)
+    })
     @PutMapping("/{recipeId}")
-    public ResponseEntity<RestResponse> updateRecipe(@PathVariable("recipeId") Long recipeId, @RequestBody @Validated RecipeDto recipeDto) {
+    public ResponseEntity<RestResponse> updateRecipe(@Parameter(description = "id of recipe to be updated")
+                                                         @PathVariable("recipeId") Long recipeId,
+                                                     @Parameter(description = "Recipe Object to be updated")
+                                                             @RequestBody @Validated RecipeDto recipeDto) {
         LOG.info("Update Recipe with ID: " + recipeId);
         return ResponseEntity.ok(recipeService.updateRecipe(recipeId, recipeDto));
     }
 
+    @Operation(summary = "Delete recipe by its id")
+    @ApiResponses( value = {
+            @ApiResponse(responseCode = "204", description = "Recipe deleted",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Recipe.class))}),
+            @ApiResponse(responseCode = "404", description = "No Recipe data found with given ID",
+                    content = @Content),
+            @ApiResponse(responseCode = "400" , description = "Recipe could not deleted",
+                    content= @Content)
+    })
     @DeleteMapping("/{recipeId}")
-    public ResponseEntity<RestResponse> deleteRecipe(@PathVariable("recipeId") Long recipeId) {
+    public ResponseEntity<RestResponse> deleteRecipe(@Parameter(description = "id of recipe to be updated")
+                                                         @PathVariable("recipeId") Long recipeId) {
         LOG.info("Delete Recipe with ID: " + recipeId);
         return ResponseEntity.ok(recipeService.deleteRecipe(recipeId));
     }
 
+    @Operation(summary = "Find recipe by its id")
+    @ApiResponses( value = {
+            @ApiResponse(responseCode = "200", description = "Recipe found",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Recipe.class))}),
+            @ApiResponse(responseCode = "404", description = "No Recipe data found with given ID",
+                    content = @Content),
+            @ApiResponse(responseCode = "400" , description = "Recipe could not found",
+                    content= @Content)
+    })
     @GetMapping("/{recipeId}")
-    public ResponseEntity<RestResponse> getRecipeById(@PathVariable("recipeId") Long recipeId) {
+    public ResponseEntity<RestResponse> getRecipeById(@Parameter(description = "id of recipe to be updated")
+                                                          @PathVariable("recipeId") Long recipeId) {
         LOG.info("Get Recipe By Id");
         return ResponseEntity.ok(recipeService.findRecipe(recipeId));
     }
 
+    @Operation(summary = "Search recipes with criteria")
+    @ApiResponse(responseCode = "200", description = "Found the recipes",
+            content = { @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Recipe.class))})
     @GetMapping("/search")
-    public ResponseEntity<RestResponse> searchRecipes(@RequestParam(value = "isVegetarian", required = false) Boolean isVegetarian,
-                                                     @RequestParam(value = "servingNumber", required = false) Integer servingNumber,
-                                                     @RequestParam(value = "ingredientName", required = false) String ingredientName,
-                                                     @RequestParam(value = "excludeIngredientName", required = false) String excludeIngredientName,
-                                                     @RequestParam(value = "instructionText", required = false) String instructionText) {
+    public ResponseEntity<RestResponse> searchRecipes(@Parameter(description = "is Recipe vegetarian")
+                                                          @RequestParam(value = "isVegetarian", required = false) Boolean isVegetarian,
+                                                      @Parameter(description = "Number of servings of recipe")
+                                                          @RequestParam(value = "servingNumber", required = false) Integer servingNumber,
+                                                      @Parameter(description = "ingredient to be included")
+                                                          @RequestParam(value = "ingredientName", required = false) String ingredientName,
+                                                      @Parameter(description = "ingredient to be excluded")
+                                                          @RequestParam(value = "excludeIngredientName", required = false) String excludeIngredientName,
+                                                      @Parameter(description = "instruction within recipe")
+                                                          @RequestParam(value = "instructionText", required = false) String instructionText) {
 
         List<SearchCriteria> criteriaList = new ArrayList<>();
 
--- src/main/java/nl/abnamro/assessment/recipe/service/RecipeSpecification.java
+++ src/main/java/nl/abnamro/assessment/recipe/service/RecipeSpecification.java
@@ -8,7 +8,9 @@ import org.springframework.data.jpa.domain.Specification;
 
 import javax.persistence.criteria.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author Orhan Polat
@@ -31,33 +33,34 @@ public class RecipeSpecification implements Specification<Recipe> {
 
     @Override
     public Predicate toPredicate(Root<Recipe> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+
         List<Predicate> predicates = new ArrayList<>();
-        Join<Recipe, Ingredients> recipeIngredients = ingredientsJoin(root);
-        Join<Recipe, Instructions> recipeInstructions = instructionsJoin(root);
 
         for (SearchCriteria criteria : criteriaList) {
-            if (criteria.getOperation() == SearchOperation.EQUALITY) {
+            if (SearchOperation.EQUALITY.equals(criteria.getOperation())) {
                 predicates.add(criteriaBuilder.equal(root.get(criteria.getKey()), criteria.getValue()));
 
-            } else if (criteria.getOperation() == SearchOperation.IN) {
+            } else if (SearchOperation.IN.equals(criteria.getOperation())) {
                 predicates.add(criteriaBuilder.in(root.get(criteria.getKey())).value(criteria.getValue()));
 
-            } else if (criteria.getOperation() == SearchOperation.NOT_IN) {
+            } else if (SearchOperation.NOT_IN.equals(criteria.getOperation())) {
                 predicates.add(criteriaBuilder.not(root.get(criteria.getKey())).in(criteria.getValue()));
 
-            } else if (criteria.getOperation() == SearchOperation.LIKE) {
+            } else if (SearchOperation.LIKE.equals(criteria.getOperation())) {
                 predicates.add(criteriaBuilder.like(root.get(criteria.getKey()), "%" + criteria.getValue() + "%"));
 
-            } else if (criteria.getOperation() == SearchOperation.GREATER_THAN_OR_EQUAL) {
+            } else if (SearchOperation.GREATER_THAN_OR_EQUAL.equals(criteria.getOperation())) {
                 predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get(criteria.getKey()), criteria.getValue().toString()));
 
             } else if (SearchOperation.CONTAINS.equals(criteria.getOperation())) {
 
                 if ("ingredientName".equals(criteria.getKey())) {
+                    Join<Recipe, Ingredients> recipeIngredients = getExistingJoin(root, "ingredients", Ingredients.class);
                     predicates.add(criteriaBuilder.like(criteriaBuilder.lower(recipeIngredients.get(criteria.getKey())),
                             "%" + criteria.getValue() + "%"));
 
                 } else if ("description".equals(criteria.getKey())){
+                    Join<Recipe, Instructions> recipeInstructions = getExistingJoin(root, "instructions", Instructions.class);
                     predicates.add(criteriaBuilder.like(criteriaBuilder.lower(recipeInstructions.get(criteria.getKey())),
                             "%" + criteria.getValue() + "%"));
 
@@ -68,10 +71,12 @@ public class RecipeSpecification implements Specification<Recipe> {
             } else if (SearchOperation.NOT_CONTAINS.equals(criteria.getOperation())) {
 
                 if ("ingredientName".equals(criteria.getKey())) {
+                    Join<Recipe, Ingredients> recipeIngredients = getExistingJoin(root, "ingredients", Ingredients.class);
                     predicates.add(criteriaBuilder.notLike(criteriaBuilder.lower(recipeIngredients.get(criteria.getKey())),
                             "%" + criteria.getValue() + "%"));
 
                 } else if ("description".equals(criteria.getKey())){
+                    Join<Recipe, Instructions> recipeInstructions = getExistingJoin(root, "instructions", Instructions.class);
                     predicates.add(criteriaBuilder.notLike(criteriaBuilder.lower(recipeInstructions.get(criteria.getKey())),
                             "%" + criteria.getValue() + "%"));
 
@@ -87,11 +92,24 @@ public class RecipeSpecification implements Specification<Recipe> {
         return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
     }
 
-    private Join<Recipe, Ingredients> ingredientsJoin(Root<Recipe> root) {
-        return root.join("ingredients");
-    }
-
-    private Join<Recipe, Instructions> instructionsJoin(Root<Recipe> root) {
-        return root.join("instructions");
+    public <R, J> Join<R, J> getExistingJoin(Root<R> root, String attribute, Class<J> joinedEntity) {
+        Join<R, J> genericJoin = null;
+        final Set<Join<R, ?>> joins = root.getJoins();
+        if (!joins.isEmpty()) {
+            final Iterator<Join<R, ?>> iterator = joins.iterator();
+            while (iterator.hasNext()) {
+                final Join<R, ?> next = iterator.next();
+                if (next.getModel().getBindableJavaType().getSimpleName().equals(joinedEntity.getSimpleName())) {
+                    genericJoin = (Join<R, J>) next;
+                    break;
+                }
+            }
+            if (genericJoin == null) {
+                genericJoin = root.join(attribute);
+            }
+        } else {
+            genericJoin = root.join(attribute);
+        }
+        return genericJoin;
     }
 }
